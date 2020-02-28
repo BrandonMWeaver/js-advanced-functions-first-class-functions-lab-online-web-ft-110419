4d6ba485b36810dda6260cc8bc355acdbfb36a4c
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 16:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const returnLastTwoDrivers = function(drivers) {
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+  return function(fare) {
+    return fare * integer;
+  };
+}
